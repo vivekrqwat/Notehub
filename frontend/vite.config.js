16c8e2c8324243a77ac035dd5d208dc25232e957
vivek <136249117+vivekrqwat@users.noreply.github.com>
2025-07-27 23:41:03 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{'/apii':{
-      target:'https://notehub-1-v9ea.onrender.com',
-      changeOrigin: true,
-        secure: false,
+  // server:{
+  //   proxy:{'/apii':{
+  //     target:'https://notehub-1-v9ea.onrender.com',
+  //     changeOrigin: true,
+  //       secure: false,
 
-    }
-  }
-  }
+  //   }
+  // }
+  // }
 })
